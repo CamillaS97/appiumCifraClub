1c48f8d83a05c6d979e536bee9a88b0cdf1c2485
DuduSouzaVeiga <esv@cesar.school>
2025-01-25 13:47:51 -0300

    added comment

--- test_cifra_club.py
+++ test_cifra_club.py
@@ -6,6 +6,14 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# Membros da equipe:
+# Camilla Silvestre
+# Eduardo Souza
+# Helena Juliana
+# Tainah Regueira
+# Aplicativo escolhido para o teste: Cifra Club
+
+# try catch para otimizar o WebDriverWait
 def element_exist(driver, element):
    try:
        wait_time = 1
@@ -14,7 +22,7 @@ def element_exist(driver, element):
    except:
        return False
 
-
+# Configuração inicial do appium
 class TestCifraClub(unittest.TestCase):
     def setUp(self):
         options = AppiumOptions()
@@ -25,27 +33,34 @@ class TestCifraClub(unittest.TestCase):
         self.driver = webdriver.Remote("http://127.0.0.1:4723", options=options)
         self.wait_time = 10
 
+# TearDown/Quit
     def tearDown(self):
         if self.driver:
             self.driver.quit()
 
+# Teste 1 | Funcionalidade: barra de pesquisa | Cenário: deve buscar e acessar uma música (Hotel California)
     def test_01_search_song(self):
+        # Acessa e clica na barra de pesquisa
         search_btn = self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Search")
         search_btn.click()
+        # Digita o nome da música e clica no primeiro resultado
         WebDriverWait(self.driver, self.wait_time).until(EC.presence_of_element_located((AppiumBy.CLASS_NAME, "android.widget.EditText")))
         self.driver.find_element(by=AppiumBy.CLASS_NAME, value="android.widget.EditText").send_keys("Hotel California")
         WebDriverWait(self.driver, self.wait_time).until(EC.presence_of_element_located((AppiumBy.XPATH, '(//android.widget.RelativeLayout[@resource-id="com.studiosol.cifraclub:id/cellClickableArea"])[1]/android.widget.LinearLayout')))
         choose_song = self.driver.find_element(by=AppiumBy.XPATH,
                                   value='(//android.widget.RelativeLayout[@resource-id="com.studiosol.cifraclub:id/cellClickableArea"])[1]/android.widget.LinearLayout')
         choose_song.click()
+        # Assert para garantir que a música foi de fato selecionada
         WebDriverWait(self.driver, self.wait_time).until(EC.presence_of_element_located((AppiumBy.ID, 'com.studiosol.cifraclub:id/songListenLayout')))
         song_title = self.driver.find_element(by=AppiumBy.XPATH, value='//android.widget.TextView[@text="Hotel California"]')
         self.assertEqual("Hotel California", song_title.text)
 
-
+# Teste 2 | Funcionalidade: configurações da cifra | Cenário: deve alterar a posição do capotraste na cifra escolhida
     def test_02_change_capo(self):
+        # Acessa a aba de configurações de uma cifra
         settings_btn = self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Settings")
         settings_btn.click()
+        # Acessa o seletor de capotraste e seleciona a opção "1st fret" 
         capo_option = self.driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR,value='new UiSelector().text("Capo")')
         capo_option.click()
         WebDriverWait(self.driver, self.wait_time).until(
@@ -55,24 +70,32 @@ class TestCifraClub(unittest.TestCase):
         fret_option.click()
         ok_btn = self.driver.find_element(by=AppiumBy.ID, value="com.studiosol.cifraclub:id/positiveBt")
         ok_btn.click()
+        # Retorna à cifra
         settings_btn = self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Settings")
         settings_btn.click()
+        # Assert para garantir que houve alteração de capotraste
         capo_check = self.driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR,
                                               value="new UiSelector().text(\"Capo: 1st fret\")")
         self.assertEqual("Capo: 1st fret", capo_check.text)
 
+# Teste 3 | Funcionalidade: configurações da cifra | Cenário: deve alterar o tom de uma cifra
     def test_03_change_tone(self):
+        # Acessa a aba de configurações de uma cifra
         tone_options = self.driver.find_element(by=AppiumBy.ID, value="com.studiosol.cifraclub:id/songToneTxt")
         tone_options.click()
+        # Acessa o seletor de tonalidade da cifra e seleciona a opção "TONE: E"
         WebDriverWait(self.driver, self.wait_time).until(EC.presence_of_element_located((AppiumBy.ID, "com.studiosol.cifraclub:id/tone7")))
         chosen_tone = self.driver.find_element(by=AppiumBy.ID, value="com.studiosol.cifraclub:id/tone7")
         chosen_tone.click()
         ok_btn = self.driver.find_element(by=AppiumBy.ID, value="com.studiosol.cifraclub:id/positiveBt")
         ok_btn.click()
+        # Assert para garantir alteração de tom
         updated_result = self.driver.find_element(by=AppiumBy.ID, value="com.studiosol.cifraclub:id/songToneTxt")
         self.assertEqual("TONE: E", updated_result.text)
 
+# Teste 4 | Funcionalidade: barra de pesquisa | Cenário: deve limpar a barra de pesquisa
     def test_04_clear_search(self):
+        # Acessa a barra de pesquisa e clica em "Clear history"
         if not element_exist(self.driver, (AppiumBy.ACCESSIBILITY_ID, "Navigate up")):
             search_btn = self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Search")
             search_btn.click()
@@ -89,7 +112,7 @@ class TestCifraClub(unittest.TestCase):
             clear_btn = self.driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().text(\"Clear history\")")
             clear_btn.click()
 
+        # Checa tamanho da lista para garantir que está com len = 1, o que significa que a lista foi limpa
         list = self.driver.find_element(AppiumBy.ID, "com.studiosol.cifraclub:id/listView")
         child_elements = list.find_elements(AppiumBy.XPATH, ".//*")
-        #Obs: printa um que eh o proprio listview, significa que a lista esta vazia
         self.assertEqual(1, len(child_elements))
